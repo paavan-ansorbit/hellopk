59ca5cab789f6d30bf903324b5954d76c46abe25
Kishan Mundha <kishan@tokeet.com>
2023-02-15 18:25:14 +0530

    static removed

--- hellopk/settings.py
+++ hellopk/settings.py
@@ -132,5 +132,18 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_DIRS = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+# {
+# "src": "build_files.sh",
+# "use":"@vercel/static-build",
+# "config": {
+#     "distDir": "staticfiles_build"
+# }
+# }
+
+    # {
+    #   "src": "/static/(.*)",
+    #   "dest": "/static/$1"
+    # },
